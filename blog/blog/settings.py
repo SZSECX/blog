eae01ccc4fc6e087bc70b6015a5acef31edbff10
szsecx <2424774178@qq.com>
2021-07-18 19:25:11 +0800

    '数据库配置'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#  }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'itbit_blog',
+        'USER': 'root',
+        'PASSWORD': '20001208ye',
+        'HOST': '127.0.0.1',
+        'POST': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
